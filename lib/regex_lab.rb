c1a113667f313b9292412ca4ad0582033472a4d5
Suchitra Naik <susanaik@gmail.com>
2018-05-01 18:34:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,42 @@
-def starts_with_a_vowel?(word)
+require 'pry' 
 
+def starts_with_a_vowel?(word)
+ vowel = word.match(/^[aeiouAEIOU]/) 
+ if vowel == nil 
+   return false 
+ else 
+   return true 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
-end
+  arr1 = []
+  arr1 = text.split(" ")
+  
+  arr2 = text.scan(/\w{5}/)
+ 
+ 
+ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
-
+   puts phone
+   
+   #phone.scan(/[!0-9]/)
+   phone.delete "!@#$%^&*()A-Za-z-=+"
+   puts phone
+   if(phone.length == 10)
+     return true
+   else 
+     return false
+   end 
+   
+   
 end
